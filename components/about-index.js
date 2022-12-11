93310e9af787f52df377af61ab22869ef069c3ef
Zac-Zajdel <zaczajdel213@gmail.com>
2022-12-10 23:59:10 -0600

    feat: Componetize Spotify Card

--- components/about-index.js
+++ components/about-index.js
@@ -1,11 +1,7 @@
 import Image from './image'
-import useSWR from 'swr'
-import { SiSpotify } from 'react-icons/si'
+import SpotifyCard from './spotifyCard'
 
 export default function AboutIndex() {
-  const fetcher = (url) => fetch(url).then((r) => r.json())
-  const { data } = useSWR('/api/spotify', fetcher)
-
   return (
     <>
       <div>
@@ -33,47 +29,8 @@ export default function AboutIndex() {
               Indiana University of Pennsylvania
             </div>
             <div>
-              <section className="">
-                <a
-                  target="_blank"
-                  rel="noopener noreferer noreferrer"
-                  href={
-                    data?.isPlaying
-                      ? data.songUrl
-                      : 'https://open.spotify.com/user/zaczajdel213'
-                  }
-                  className="relative flex items-center space-x-4 rounded-md pl-2 p-5 transition-shadow hover:shadow-md no-underline"
-                >
-                  <div className="w-16">
-                    {data?.isPlaying ? (
-                      <Image
-                        src={data?.albumImageUrl}
-                        alt={data?.album}
-                        width={80}
-                        height={80}
-                        className="w-16 shadow-sm"
-                      />
-                    ) : (
-                      <SiSpotify size={64} color={'#1ED760'} />
-                    )}
-                  </div>
-
-                  <div className="flex-1">
-                    <p className="text-sm font-bold text-ellipsis overflow-hidden">
-                      {data?.isPlaying ? data.title : 'Not Listening'}
-                    </p>
-                    <span className="font-dark text-xs flex justify-between items-center pt-2">
-                      <span>{data?.isPlaying ? data.artist : 'Spotify'}</span>
-                      <span>
-                        {data?.isPlaying ? (
-                          <div className="pl-2">
-                            <SiSpotify size={20} color={'#1ED760'} />
-                          </div>
-                        ) : null}
-                      </span>
-                    </span>
-                  </div>
-                </a>
+              <section>
+                <SpotifyCard />
               </section>
             </div>
           </div>
