04919b73f46be1da57d991f849b5814d7d01ba06
Zachary Zajdel <zaczajdel213@gmail.com>
2025-06-25 22:24:03 -0500

    feat: adjust project cards

--- lib/zod/views.ts
+++ lib/zod/views.ts
@@ -2,12 +2,12 @@ import { z } from 'zod';
 
 export const getViewsSchema = () => {
   return z.object({
-    slug: z.enum(['oasis', 'jottings']),
+    slug: z.enum(['oasis', 'vibe-log']),
   });
 };
 
 export const storeViewsSchema = () => {
   return z.object({
-    slug: z.enum(['oasis', 'jottings']),
+    slug: z.enum(['oasis', 'vibe-log']),
   });
 };
