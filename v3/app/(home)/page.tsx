fd613fc570fdaa983e2712a776a5a7f1e54d2d89
Zachary Zajdel <zaczajdel213@gmail.com>
2025-11-27 18:32:17 -0600

    feat: setup home page and cleaned up icon components

--- v3/app/(home)/page.tsx
+++ v3/app/(home)/page.tsx
@@ -1,19 +1,96 @@
-import Link from 'next/link';
+import Discord from '@/components/icons/discord';
+import Github from '@/components/icons/github';
+import Linkedin from '@/components/icons/linkedin';
+import Pulse from '@/components/pulse';
+import Image from 'next/image';
+import { MdMailOutline } from 'react-icons/md';
 
 export default function HomePage() {
+  const socialLinks = [
+    {
+      href: 'https://github.com/Zac-Zajdel',
+      label: 'Follow me on GitHub',
+      Icon: Github,
+      className: 'size-6 fill-black dark:fill-white',
+    },
+    {
+      href: 'mailto:zaczajdel213@gmail.com',
+      label: 'Send me an email',
+      Icon: MdMailOutline,
+      className: 'size-6',
+    },
+    {
+      href: 'https://www.linkedin.com/in/zachary-zajdel-9b832b191',
+      label: 'Follow me on LinkedIn',
+      Icon: Linkedin,
+      className: 'size-6',
+    },
+    {
+      href: 'https://discord.com/users/ZacZajdel#5036',
+      label: 'Follow me on Discord',
+      Icon: Discord,
+      className: 'size-6 fill-black dark:fill-white',
+    },
+  ];
+
   return (
-    <div className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link
-          href="/blogs"
-          className="font-medium underline"
-        >
-          /blogs
-        </Link>{' '}
-        and see the blogs.
-      </p>
-    </div>
+    <main className="xl:px-42 container mx-auto mt-20 px-5">
+      <div className="lg:grid lg:grid-cols-3 lg:place-items-center lg:gap-16">
+        <div className="lg:col-span-2">
+          <h1 className="text-4xl tracking-tight md:text-5xl">Zac Zajdel</h1>
+          <p className="pt-4 text-fd-muted-foreground">
+            Full-Stack Software Engineer
+          </p>
+          <p className="pt-3">
+            Hi there! I&apos;m a full-stack web developer with a passion for
+            turning ideas into reality. I love the challenge of building from
+            the ground up and constantly learning new tools and technologies.
+            Take a look around and feel free to connect with me through my
+            socials below! 😊
+          </p>
+
+          <ul
+            className="mt-5 flex items-center gap-6"
+            aria-label="Social links"
+          >
+            {socialLinks.map(({ href, label, Icon, className }) => (
+              <li key={label}>
+                <a
+                  className="inline-flex items-center rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-current focus-visible:ring-offset-2"
+                  aria-label={label}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={href}
+                >
+                  <Icon className={className} />
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <div className="pt-6 text-sm">Dallas, TX · 32.775° N, -96.809° W</div>
+        </div>
+
+        <div className="mt-10 lg:col-span-1 lg:mt-0">
+          <div className="grid place-items-center">
+            <Image
+              src="/static/me.jpg"
+              alt="Photo of Zac Zajdel"
+              width={192}
+              height={192}
+              className="rounded-full"
+              priority
+              sizes="192px"
+            />
+            <p className="mt-5 flex items-center">
+              <Pulse aria-hidden />
+              <span className="pl-1 text-sm">
+                Interested in new opportunities
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
--- v3/app/layout.tsx
+++ v3/app/layout.tsx
@@ -1,4 +1,5 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './global.css';
 
@@ -6,6 +7,52 @@ const inter = Inter({
   subsets: ['latin'],
   preload: false,
 });
+
+const META_URL =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:3000'
+    : 'https://www.zaczajdel.com/';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(META_URL),
+  title: 'Learn With Zac',
+  description: 'Portfolio Website of Zac Zajdel.',
+  applicationName: 'Learn With Zac',
+  authors: [{ name: 'Zac', url: 'https://www.zaczajdel.com/' }],
+  keywords: ['Blog', 'Zac Zajdel', 'Coding', 'Programmer', 'Software Engineer'],
+  openGraph: {
+    title: 'Learn With Zac',
+    description: 'Portfolio Website of Zac Zajdel.',
+    siteName: 'Learn With Zac',
+    images: '/android-chrome-192x192.png',
+    url: 'https://www.zaczajdel.com/',
+  },
+  twitter: {
+    title: 'Learn With Zac',
+    description: 'Portfolio Website of Zac Zajdel.',
+    creator: '@zac_zajdel',
+  },
+  icons: [
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: `${META_URL}/favicon-32x32.png`,
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: `${META_URL}/favicon-16x16.png`,
+    },
+  ],
+  manifest: '/site.webmanifest',
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
